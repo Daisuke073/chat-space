68bd05dfcfcaad8384876518a4274f08f1ce4070
Daisuke073 <cpxyh073@gmail.com>
2020-09-06 17:55:18 +0900

    json形式の条件分岐

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,16 @@
 $(function(){
-  console.log("hello")
+  $('.ChatForm').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    console.log(1)
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
 });
